e60500358c7ebd379fbbe45fd466e7fea8e75c41
Solomon sokoya <solomonsokoya@Solomons-MBP.fios-router.home>
2018-06-22 21:05:05 -0400

    Added files to work on handling actions with redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
-import { createStore } from "redux";
-import reducer from "./reducers"
-
-const initialState = {tech: 'React'};
-const store = createStore(reducer, initialState);
+import ButtonGroup from "./ButtonGroup";
+import { store } from "./store";
 
 class App extends Component {
 
   render() {
-    return <HelloWorld tech={store.getState().tech} />;
+    return [
+
+      <HelloWorld key={1} tech={store.getState().tech} />
+      <ButtonGroup key={2} technologies={["React", "Elm", "Reactredux"]}/>
+
+  ]
+
   }
 }
 
---
+++ src/ButtonGroup.js
@@ -0,0 +1,8 @@
+import React from "react";
+const ButtonGroup = ({technologies}) => (<div>
+  {
+    technologies.map((tech, i) => (<button data-btn={tech} key={`btn-${i}`} className="hello-btn">
+      {tech}
+    </button>))
+  }
+</div>);
