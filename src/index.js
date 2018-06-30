e8037f623f63c26da3278ad298cec0be437366a3
Solomon sokoya <solomonsokoya@Solomons-MBP.home>
2018-06-30 13:18:38 -0400

    Installed react-redux for connect and provider

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import './index.css';
 import App from './App';
 import {store} from './store';
 import registerServiceWorker from './registerServiceWorker';
 
-const render = () => ReactDOM.render(<App/>, document.getElementById("root"));
-render();
-store.subscribe(render);
+const Dom = (
+  <Provider store = {store}>
+    <App/>
+  </Provider>
+)
+ReactDOM.render(Dom, document.getElementById("root"));
